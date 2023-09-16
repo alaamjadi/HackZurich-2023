99bd2b4a797e53799ad2b08124cf8cc1fbb4047f
Swetha <62414900+ms-geeky@users.noreply.github.com>
2023-09-16 13:46:59 +0200

    Added more file formats(png, jpg, bmp, gif, jpeg)

--- app/naz-temp/naz-test.py
+++ app/naz-temp/naz-test.py
@@ -1,11 +1,29 @@
 import pytesseract
 from PIL import Image
 
-# Open an image
-image = Image.open('temp.jpg')
+image_file = 'img.png'  # Change this to the path of your image file
 
-# Perform OCR
-text = pytesseract.image_to_string(image)
+# Function to perform OCR on an image
+def ocr_image(image_path):
+    try:
+        # Open an image
+        image = Image.open(image_path)
 
-# Print the extracted text
-print(text)
+        # Perform OCR
+        text = pytesseract.image_to_string(image)
+
+        return text
+    except Exception as e:
+        return str(e)
+
+# List of image file extensions to process
+image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+
+
+# Check if the file extension is in the list of supported extensions
+file_extension = image_file.lower()
+if any(file_extension.endswith(ext) for ext in image_extensions):
+    extracted_text = ocr_image(image_file)
+    print(extracted_text)
+else:
+    print("Unsupported image file format")
