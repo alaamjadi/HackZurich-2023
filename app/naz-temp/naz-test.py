fb28d5e4f31a54a99be5083ce0ca19680ede4c08
XavierBlake <Nazish.Malik@warwick.ac.uk>
2023-09-16 09:05:38 +0100

    Pushing temp testing program

---
+++ app/naz-temp/naz-test.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import sys
+import pyocr
+import pyocr.builders
+
+
+# im = Image.open("tmp.jpg")
+# im = im.filter(ImageFilter.MedianFilter())
+# enhancer = ImageEnhance.Contrast(im)
+# im = enhancer.enhance(2)
+# im = im.convert('1')
+# im.save("temp.jpg")
+
+# print(pytesseract.image_to_string("temp.jpg"))
